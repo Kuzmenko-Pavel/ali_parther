2780035f8d60c6387c2a9cbac92525f9fa818d9b
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2020-01-27 10:16:38 +0200

    change link

--- ali_partner/views/api.py
+++ ali_partner/views/api.py
@@ -11,42 +11,43 @@ gotbest_range = 1.0
 
 class ApiView(web.View):
     async def get_data(self, processed=None):
-        if processed is None:
-            return web.Response(body='')
-
-        if self.request.fail_referer or self.request.bot or self.request.is_customer:
-            static_path = os.path.join(self.request.app['config']['dir_path'], 'static')
-            tail = self.request.match_info['tail']
-            file_path = os.path.join(static_path, tail)
-            file_exists = os.path.isfile(file_path)
-            if not file_exists:
-                file_path = os.path.join(static_path, 'index.html')
-            logger.info('fail_referer %s or user-agent %s or ip %s on %s' % (
-                self.request.referer, self.request.user_agent, self.request.ip, tail
-            ))
-            return web.FileResponse(path=file_path)
-
-        if self.request.ali_visited:
-            return web.Response(body='')
-
-        partner = choice(list(partner_links.keys()))
-        partner_link = partner_links[partner]['partner_link']
-        partner_offers = partner_links[partner]['partner_offers']
-        count_gotbest = self.request.app.view_count['gotbest']
-        count_aliexpress = self.request.app.view_count['aliexpress']
-        if (count_gotbest + count_aliexpress) % 1000 == 0:
-            logger.info(('gotbest %s aliexpress %s' % (count_gotbest, count_aliexpress)))
-
-        if count_gotbest > ((count_gotbest + count_aliexpress) / 100.0) * gotbest_range:
-                self.request.app.view_count['aliexpress'] += 1
-                return web.HTTPFound(choice(partner_offers))
-        else:
-            if self.request.partner_visited:
-                self.request.app.view_count['aliexpress'] += 1
-                return web.HTTPFound(choice(partner_offers))
-            else:
-                self.request.app.view_count['gotbest'] += 1
-                return web.HTTPFound(partner_link)
+        return web.Response(body='')
+        # if processed is None:
+        #     return web.Response(body='')
+        #
+        # if self.request.fail_referer or self.request.bot or self.request.is_customer:
+        #     static_path = os.path.join(self.request.app['config']['dir_path'], 'static')
+        #     tail = self.request.match_info['tail']
+        #     file_path = os.path.join(static_path, tail)
+        #     file_exists = os.path.isfile(file_path)
+        #     if not file_exists:
+        #         file_path = os.path.join(static_path, 'index.html')
+        #     logger.info('fail_referer %s or user-agent %s or ip %s on %s' % (
+        #         self.request.referer, self.request.user_agent, self.request.ip, tail
+        #     ))
+        #     return web.FileResponse(path=file_path)
+        #
+        # if self.request.ali_visited:
+        #     return web.Response(body='')
+        #
+        # partner = choice(list(partner_links.keys()))
+        # partner_link = partner_links[partner]['partner_link']
+        # partner_offers = partner_links[partner]['partner_offers']
+        # count_gotbest = self.request.app.view_count['gotbest']
+        # count_aliexpress = self.request.app.view_count['aliexpress']
+        # if (count_gotbest + count_aliexpress) % 1000 == 0:
+        #     logger.info(('gotbest %s aliexpress %s' % (count_gotbest, count_aliexpress)))
+        #
+        # if count_gotbest > ((count_gotbest + count_aliexpress) / 100.0) * gotbest_range:
+        #         self.request.app.view_count['aliexpress'] += 1
+        #         return web.HTTPFound(choice(partner_offers))
+        # else:
+        #     if self.request.partner_visited:
+        #         self.request.app.view_count['aliexpress'] += 1
+        #         return web.HTTPFound(choice(partner_offers))
+        #     else:
+        #         self.request.app.view_count['gotbest'] += 1
+        #         return web.HTTPFound(partner_link)
 
     async def get(self):
         return await self.get_data(True)
